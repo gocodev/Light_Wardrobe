f6b351f9a7fbb8851f9d206e32f4f7ef3b66d5bf
rabee <rabee03@gmail.com>
2017-09-14 22:26:54 +0200

    Adding output pins setup
    Adding setup of pins of expanders

--- source/light_wardrobe/light_wardrobe.ino
+++ source/light_wardrobe/light_wardrobe.ino
@@ -17,6 +17,11 @@ void setup() {
   IN_expander_3.begin(0x22);
   IN_expander_4.begin(0x23);
 
+  OUT_expander_1.begin(0x24);
+  OUT_expander_1.begin(0x25);
+  OUT_expander_1.begin(0x26);
+  OUT_expander_1.begin(0x27);
+
   IN_expander_1.pinMode(0, INPUT);
   IN_expander_1.pinMode(1, INPUT);
   IN_expander_1.pinMode(2, INPUT);
@@ -52,6 +57,42 @@ void setup() {
   IN_expander_4.pinMode(5, INPUT);
   IN_expander_4.pinMode(6, INPUT);
   IN_expander_4.pinMode(7, INPUT);
+
+  OUT_expander_1.pinMode(0, OUTPUT);
+  OUT_expander_1.pinMode(1, OUTPUT);
+  OUT_expander_1.pinMode(2, OUTPUT);
+  OUT_expander_1.pinMode(3, OUTPUT);
+  OUT_expander_1.pinMode(4, OUTPUT);
+  OUT_expander_1.pinMode(5, OUTPUT);
+  OUT_expander_1.pinMode(6, OUTPUT);
+  OUT_expander_1.pinMode(7, OUTPUT);
+
+  OUT_expander_2.pinMode(0, OUTPUT);
+  OUT_expander_2.pinMode(1, OUTPUT);
+  OUT_expander_2.pinMode(2, OUTPUT);
+  OUT_expander_2.pinMode(3, OUTPUT);
+  OUT_expander_2.pinMode(4, OUTPUT);
+  OUT_expander_2.pinMode(5, OUTPUT);
+  OUT_expander_2.pinMode(6, OUTPUT);
+  OUT_expander_2.pinMode(7, OUTPUT);
+
+  OUT_expander_3.pinMode(0, OUTPUT);
+  OUT_expander_3.pinMode(1, OUTPUT);
+  OUT_expander_3.pinMode(2, OUTPUT);
+  OUT_expander_3.pinMode(3, OUTPUT);
+  OUT_expander_3.pinMode(4, OUTPUT);
+  OUT_expander_3.pinMode(5, OUTPUT);
+  OUT_expander_3.pinMode(6, OUTPUT);
+  OUT_expander_3.pinMode(7, OUTPUT);
+
+  OUT_expander_4.pinMode(0, OUTPUT);
+  OUT_expander_4.pinMode(1, OUTPUT);
+  OUT_expander_4.pinMode(2, OUTPUT);
+  OUT_expander_4.pinMode(3, OUTPUT);
+  OUT_expander_4.pinMode(4, OUTPUT);
+  OUT_expander_4.pinMode(5, OUTPUT);
+  OUT_expander_4.pinMode(6, OUTPUT);
+  OUT_expander_4.pinMode(7, OUTPUT);
 
 }
 
