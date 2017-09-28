a44134f4ebdddcbc6a89185faaf998525bb18f44
rabee <rabee03@gmail.com>
2017-09-28 22:31:30 +0200

    Modify of configuration of I/O pins

--- source/light_wardrobe/light_wardrobe.ino
+++ source/light_wardrobe/light_wardrobe.ino
@@ -1,102 +1,58 @@
-#include <PCF8574.h>
 #include <Wire.h>
+#include <PCF8574.h>
 
-PCF8574 IN_expander_1;
-PCF8574 IN_expander_2;
-PCF8574 IN_expander_3;
-PCF8574 IN_expander_4;
+PCF8574 outExp4;
+PCF8574 inExp1;
 
-PCF8574 OUT_expander_1;
-PCF8574 OUT_expander_2;
-PCF8574 OUT_expander_3;
-PCF8574 OUT_expander_4;
+int stan = 0; 
 
 void setup() {
-  IN_expander_1.begin(0x20);
-  IN_expander_2.begin(0x21);
-  IN_expander_3.begin(0x22);
-  IN_expander_4.begin(0x23);
+  outExp4.begin(0x27);
+  inExp1.begin(0x20); 
+  inConfig();
+  outConfig();
+  
 
-  OUT_expander_1.begin(0x24);
-  OUT_expander_1.begin(0x25);
-  OUT_expander_1.begin(0x26);
-  OUT_expander_1.begin(0x27);
-
-  IN_expander_1.pinMode(0, INPUT);
-  IN_expander_1.pinMode(1, INPUT);
-  IN_expander_1.pinMode(2, INPUT);
-  IN_expander_1.pinMode(3, INPUT);
-  IN_expander_1.pinMode(4, INPUT);
-  IN_expander_1.pinMode(5, INPUT);
-  IN_expander_1.pinMode(6, INPUT);
-  IN_expander_1.pinMode(7, INPUT);
-
-  IN_expander_2.pinMode(0, INPUT);
-  IN_expander_2.pinMode(1, INPUT);
-  IN_expander_2.pinMode(2, INPUT);
-  IN_expander_2.pinMode(3, INPUT);
-  IN_expander_2.pinMode(4, INPUT);
-  IN_expander_2.pinMode(5, INPUT);
-  IN_expander_2.pinMode(6, INPUT);
-  IN_expander_2.pinMode(7, INPUT);
-
-  IN_expander_3.pinMode(0, INPUT);
-  IN_expander_3.pinMode(1, INPUT);
-  IN_expander_3.pinMode(2, INPUT);
-  IN_expander_3.pinMode(3, INPUT);
-  IN_expander_3.pinMode(4, INPUT);
-  IN_expander_3.pinMode(5, INPUT);
-  IN_expander_3.pinMode(6, INPUT);
-  IN_expander_3.pinMode(7, INPUT);
-
-  IN_expander_4.pinMode(0, INPUT);
-  IN_expander_4.pinMode(1, INPUT);
-  IN_expander_4.pinMode(2, INPUT);
-  IN_expander_4.pinMode(3, INPUT);
-  IN_expander_4.pinMode(4, INPUT);
-  IN_expander_4.pinMode(5, INPUT);
-  IN_expander_4.pinMode(6, INPUT);
-  IN_expander_4.pinMode(7, INPUT);
-
-  OUT_expander_1.pinMode(0, OUTPUT);
-  OUT_expander_1.pinMode(1, OUTPUT);
-  OUT_expander_1.pinMode(2, OUTPUT);
-  OUT_expander_1.pinMode(3, OUTPUT);
-  OUT_expander_1.pinMode(4, OUTPUT);
-  OUT_expander_1.pinMode(5, OUTPUT);
-  OUT_expander_1.pinMode(6, OUTPUT);
-  OUT_expander_1.pinMode(7, OUTPUT);
-
-  OUT_expander_2.pinMode(0, OUTPUT);
-  OUT_expander_2.pinMode(1, OUTPUT);
-  OUT_expander_2.pinMode(2, OUTPUT);
-  OUT_expander_2.pinMode(3, OUTPUT);
-  OUT_expander_2.pinMode(4, OUTPUT);
-  OUT_expander_2.pinMode(5, OUTPUT);
-  OUT_expander_2.pinMode(6, OUTPUT);
-  OUT_expander_2.pinMode(7, OUTPUT);
-
-  OUT_expander_3.pinMode(0, OUTPUT);
-  OUT_expander_3.pinMode(1, OUTPUT);
-  OUT_expander_3.pinMode(2, OUTPUT);
-  OUT_expander_3.pinMode(3, OUTPUT);
-  OUT_expander_3.pinMode(4, OUTPUT);
-  OUT_expander_3.pinMode(5, OUTPUT);
-  OUT_expander_3.pinMode(6, OUTPUT);
-  OUT_expander_3.pinMode(7, OUTPUT);
-
-  OUT_expander_4.pinMode(0, OUTPUT);
-  OUT_expander_4.pinMode(1, OUTPUT);
-  OUT_expander_4.pinMode(2, OUTPUT);
-  OUT_expander_4.pinMode(3, OUTPUT);
-  OUT_expander_4.pinMode(4, OUTPUT);
-  OUT_expander_4.pinMode(5, OUTPUT);
-  OUT_expander_4.pinMode(6, OUTPUT);
-  OUT_expander_4.pinMode(7, OUTPUT);
 
 }
+void outConfig(){
+  outExp4.pinMode(0, OUTPUT);
+  outExp4.pinMode(1, OUTPUT);
+  outExp4.pinMode(2, OUTPUT);
+  outExp4.pinMode(3, OUTPUT);
+  outExp4.pinMode(4, OUTPUT);
+  outExp4.pinMode(5, OUTPUT);
+  outExp4.pinMode(6, OUTPUT);
+  outExp4.pinMode(7, OUTPUT);
 
+}
+void inConfig(){  
+  inExp1.pinMode(0, INPUT);
+  inExp1.pinMode(1, INPUT);
+  inExp1.pinMode(2, INPUT);
+  inExp1.pinMode(3, INPUT);
+  inExp1.pinMode(4, INPUT);
+  inExp1.pinMode(5, INPUT);
+  inExp1.pinMode(6, INPUT);
+  inExp1.pinMode(7, INPUT);
+
+  inExp1.pullUp(0);
+  inExp1.pullUp(1);
+  inExp1.pullUp(2);
+  inExp1.pullUp(3);
+  inExp1.pullUp(4);
+  inExp1.pullUp(5);
+  inExp1.pullUp(6);
+  inExp1.pullUp(7);
+   
+}
 void loop() {
-  // put your main code here, to run repeatedly:
-
+ outExp4.digitalWrite(0, stan); 
+ outExp4.digitalWrite(1, stan);
+ outExp4.digitalWrite(2, stan); 
+ outExp4.digitalWrite(3, stan); 
+ outExp4.digitalWrite(4, stan); 
+ outExp4.digitalWrite(5, stan); 
+ outExp4.digitalWrite(6, stan); 
+ outExp4.digitalWrite(7, stan);  
 }
